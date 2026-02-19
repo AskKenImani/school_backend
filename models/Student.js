cc222beaf93546512c1d67b3df4e668a9da4bf20
Imani Kehinde <imamikenny27@gmail.com>
2026-02-19 12:55:30 +0100

    Password for student

--- models/Student.js
+++ models/Student.js
@@ -4,6 +4,7 @@ const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   role: { type: String, required: true },
   username: { type: String, required: true, unique: true, },
+  password: { type: String, required: true },
   guardian: { type: String, default: '',},
   admissionNo: { type: String, required: true, unique: true, },
   email: { type: String, required: true, unique: true },
