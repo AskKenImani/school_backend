946164d6ec67cc805f42585e79f02f72dc33eb45
Imani Kehinde <imamikenny27@gmail.com>
2026-02-23 11:28:52 +0100

    shared route fixed

--- routes/sharedRoutes.js
+++ routes/sharedRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const verifyToken = require('../middleware/verifyToken');
+const verifyToken = require('../middleware/auth');
 
 const Class = require('../models/Class');
 const Subject = require('../models/Subject');
