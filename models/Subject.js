c94dec915040f5bbddec67192eba00b5850a842d
Imani Kehinde <imamikenny27@gmail.com>
2026-02-24 13:19:40 +0100

    idempotent 2 again

--- models/Subject.js
+++ models/Subject.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const subjectSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, },
+  classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
+  teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' },
 }, { timestamps: true });
 
 const Subject = mongoose.model('Subject', subjectSchema);
